6b16f5a00310fc6e65817b39921179fc59fdb7e7
rgrishin <roman1988grishin@gmail.com>
2021-08-11 14:18:01 +0300

    Comment componenet (draft)

---
+++ confusion/src/components/CommentComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+export default function Comment (props){
+  return(
+    <Card>
+
+    </Card>
+  );
+}
